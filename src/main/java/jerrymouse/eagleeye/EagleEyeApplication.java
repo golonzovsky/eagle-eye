09e6a91940b4e57ebda6beafe1da4d2a718193e0
golonzovsky <alex.golonzovsky@gmail.com>
2017-05-20 21:52:04 +0200

    client init

--- src/main/java/jerrymouse/eagleeye/EagleEyeApplication.java
+++ src/main/java/jerrymouse/eagleeye/EagleEyeApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EagleEyeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EagleEyeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EagleEyeApplication.class, args);
+    }
 }
