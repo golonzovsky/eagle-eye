dc0ae11e6488f58ad46bf1b76b6071d8092f20ac
golonzovsky <alex.golonzovsky@gmail.com>
2017-05-27 22:14:29 +0200

    travis: inline push script

--- .travis-push-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-    docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-    docker build -f target/dockerfile/Dockerfile -t $DOCKER_USERNAME/eagle-eye:latest target
-    docker push $DOCKER_USERNAME/eagle-eye:latest
-fi
